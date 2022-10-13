1403aac41440d513b9cd2e5aab7978a3854112dc
BlessAdjorlolo <adjorlolobless11@gmail.com>
2022-10-13 20:49:28 +0000

    updates

--- pages/dashboard/patients/search-patient.js
+++ pages/dashboard/patients/search-patient.js
@@ -30,14 +30,11 @@ export default function Patient() {
           <p className=" text-gray-600 text-center text-lg mt-4">
             Enter a Patients name in the search box below and see his/her
             records pop up or click
-            <Link
-              href="/hospitals/add-patient"
+            <Link href="/hospitals/add-patient"
               className="text-cyan-500 hover:text-cyan-600 transition-duration-200 ease-in-out font-semibold cursor-pointer hover:shadow-lg "
-            >
-              {" "}
-              here
-            </Link>{" "}
-            to add new patient
+            > here
+            </Link>
+             to add new patient
           </p>
           <form onSubmit={handleSubmit}>
             <div className=" flex flex-col sm:flex-row gap-6 mt-8">
