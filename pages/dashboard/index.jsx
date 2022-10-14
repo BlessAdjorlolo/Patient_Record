f8d8eb8e40997d613bff54098578fdcd67065ab3
BlessAdjorlolo <adjorlolobless11@gmail.com>
2022-10-14 10:57:31 +0000

    up

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -24,7 +24,7 @@ const Dashboard = ({ patients }) => {
       <div className="flex flex-col-reverse lg:flex-row items-center gap-5 mt-14 lg:mt-28 ">
         <div className="px-7 flex flex-1 flex-col items-center lg:items-center">
           <h2 className=" text-3xl text-cyan-500 md:text-4xl font-bold lg:text-5xl text-center lg:text-left mb-6">
-            The world is going digital so should our health related be too
+            The world is going digital so should our health related too
           </h2>
           <p className="text-lg font-semibold text-center lg:text-left mb-6 text-gray-600">
             Ensuring easy accessibility and prevent patient delay. We present to
@@ -48,7 +48,7 @@ const Dashboard = ({ patients }) => {
             The world is going digital so should our health related be too
           </h2>
           <p className="text-lg font-semibold text-center lg:text-left mb-6 text-gray-600">
-            Ensuring easy accessibility and prevent patient delay. We present to
+            Ensuring easy accessibility and prevent patient delay. We present to you
             an era of elctronic patient record
           </p>
         </div>
@@ -59,7 +59,7 @@ const Dashboard = ({ patients }) => {
             height={500}
             width={500}
             className=" w-5/6 h-5/6 sm:h-3/4 "
-            alt="Patient Record"
+            alt=""
           />
         </div>
       </div>
